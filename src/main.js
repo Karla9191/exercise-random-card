fe2ee26fbea79412c690c30a423908f61d619d7c
Karla9191 <marietha2014@gmail.com>
2021-02-16 03:19:09 +0000

    m

--- src/main.js
+++ src/main.js
@@ -6,5 +6,48 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  console.log(tipoCarta());
+  console.log(numeroCarta());
+  document.getElementById("topIcon").innerHTML = tipoCarta();
+  document.getElementById("number").innerHTML = numeroCarta();
+  document.getElementById("bottomIcon").innerHTML = tipoCarta();
 };
+
+function tipoCarta() {
+  let figura = Math.floor(Math.random() * 4) + 1;
+  let cartaFigura = "";
+  switch (figura) {
+    case 1:
+      cartaFigura = "&spades;";
+      break;
+    case 2:
+      cartaFigura = "&clubs;";
+      break;
+    case 3:
+      cartaFigura = "&hearts;";
+      break;
+    case 4:
+      cartaFigura = "&diams;";
+      break;
+  }
+  return cartaFigura;
+}
+
+function numeroCarta() {
+  let numCarta = Math.floor(Math.random() * 13) + 1;
+  let carta = "";
+  switch (numCarta) {
+    case 11:
+      carta = "J";
+      break;
+    case 12:
+      carta = "Q";
+      break;
+    case 13:
+      carta = "K";
+      break;
+    default:
+      carta = numCarta;
+  }
+  return carta;
+}
