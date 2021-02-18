7616c1594cba7b31ef30256d0baf5e35659f4328
Karla9191 <marietha2014@gmail.com>
2021-02-18 02:13:17 +0000

    Final

--- src/main.js
+++ src/main.js
@@ -6,8 +6,23 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  console.log(tipoCarta());
-  console.log(numeroCarta());
+  /*console.log(tipoCarta());
+  console.log(numeroCarta());*/
+  let iconCard = tipoCarta();
+  //document.getElementById("topIcon").innerHTML = tipoCarta();
+  document.getElementById("numberCard").innerHTML = numeroCarta();
+  //document.getElementById("bottomIcon").innerHTML = tipoCarta();
+  if (iconCard == "&hearts;" || iconCard == "&diams;") {
+    document.getElementById("topIcon").classList.add("topIconRed");
+    document.getElementById("bottomIcon").classList.add("bottomIconRed");
+    document.getElementById("topIcon").innerHTML = iconCard;
+    document.getElementById("bottomIcon").innerHTML = iconCard;
+  } else {
+    document.getElementById("topIcon").classList.add("topIcon");
+    document.getElementById("bottomIcon").classList.add("bottomIcon");
+    document.getElementById("topIcon").innerHTML = iconCard;
+    document.getElementById("bottomIcon").innerHTML = iconCard;
+  }
 };
 
 function tipoCarta() {
